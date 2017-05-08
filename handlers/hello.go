a6a5198edf87832b36bdd10cf761ce32ea37c9d7
DieterVennekens <dieter.vennekens@atos.net>
2017-05-08 16:10:16 +0200

    Update hello.go

--- handlers/hello.go
+++ handlers/hello.go
@@ -17,7 +17,7 @@ func (p *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	styledTemplate.Execute(w, Body{Body: fmt.Sprintf(`
 <div class="hello">
-	InnovAtos 2017	
+	InnovAtos 2018	
 </div>
 
 <div class="my-index">My Index Is</div>
